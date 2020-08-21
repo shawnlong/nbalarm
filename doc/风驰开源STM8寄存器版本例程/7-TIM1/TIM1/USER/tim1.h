0eaadafeb6b2134bdfc2befed56b7a2f6084fe42
longxiang <longxiang@leadien.com>
2020-08-21 15:44:58 +0800

    move doc to ref

--- doc/风驰开源STM8寄存器版本例程/7-TIM1/TIM1/USER/tim1.h
+++
@@ -1,9 +0,0 @@
-#ifndef __TIM1_H
-#define __TIM1_H
-#include "type_def.h"
-#define TIM1_COUNTERMODE_UP  0x00
-#define TIM1_IT_UPDATE       0x01
-void Tim1_Time_Upmode_conf(uint16_t TIM1_Prescaler,uint16_t TIM1_Period,uint8_t TIM1_RepetitionCounter);
-void TimingDelay_Decrement(void);
-void delay_ms( u32 nTime);
-#endif
